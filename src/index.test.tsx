da349ebe718786c4978df91975e8d7f394521df3
XGHeaven <xgheaven@gmail.com>
2019-11-15 19:05:06 +0800

    feat: implement inject/provide

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { shallow, render, mount } from 'enzyme'
-import { regen } from './index'
+import { regen, waitTick } from './index'
+import { injectable } from 'inversify'
 
 describe('regen', () => {
   it('should works when no props passed', () => {
@@ -55,5 +56,96 @@ describe('regen', () => {
     expect(unmounted).toBeFalsy()
     wrapper.unmount()
     expect(unmounted).toBeTruthy()
+  })
+
+  it('should works when using withRender', () => {
+    const App = regen<{name: string}>(function *App({props, withRender}) {
+      yield * withRender(() => (<p>{props.name}</p>))
+    })
+    const wrapper = shallow(<App name="Iron Man"/>)
+    expect(wrapper).toContainReact(<p>Iron Man</p>)
+  })
+
+  it('should update when call mark', async () => {
+    const App = regen(function * App({mark, withRender}) {
+      let count = 1
+      const click = () => {
+        count+=1
+        mark()
+      }
+      yield * withRender(() => <p onClick={click}>{count}</p>)
+    })
+
+    const wrapper = shallow(<App/>)
+    expect(wrapper).toMatchElement(<p>{1}</p>)
+    wrapper.find('p').simulate('click')
+    expect(wrapper).toMatchElement(<p>{1}</p>)
+    await waitTick()
+    expect(wrapper).toMatchElement(<p>{2}</p>)
+  })
+
+  it('should batch update when call mark', async () => {
+    let resolve: Function
+    const waiter = new Promise((res, _) => {resolve = res})
+    const App = regen(function * App({mark, withRender}) {
+      let count = 0
+      const asyncHandler = () => {
+        mark()
+        setTimeout(() => {
+          mark()
+          mark()
+          resolve()
+        })
+      }
+
+      yield * withRender(() => {
+        count += 1;
+        return <p onClick={asyncHandler}>{count}</p>
+      })
+    })
+
+    const wrapper = shallow(<App/>)
+    expect(wrapper).toMatchElement(<p>{1}</p>)
+    wrapper.find('p').simulate('click')
+    await waiter
+    await waitTick()
+    expect(wrapper.text()).not.toEqual('4')
+  })
+
+  it('should inject works', () => {
+    @injectable()
+    class Store {
+      name = 'Jarvis'
+    }
+
+    const App = regen(function *App({withRender, provide}) {
+      provide(Store)
+
+      yield * withRender(() => <Child/>)
+    })
+
+    const Child = regen(function * Child({withRender, inject}) {
+      const store = inject(Store)
+
+      yield * withRender(() => <p>{store.name}</p>)
+    })
+
+    const wrapper = mount(<App/>)
+    expect(wrapper).toContainReact(<p>Jarvis</p>)
+  })
+
+  it.only('should custom instance method', () => {
+    const focus = jest.fn()
+    const App = regen<{}, {
+      focus: () => void
+    }>(function *App({handler, withRender}) {
+      handler.focus = focus
+      yield <p>1</p>
+    })
+
+    let inst: InstanceType<typeof App> | null
+    const wrapper = shallow(<App ref={node => {inst = node as any}}/>)
+    inst!?.focus()
+    expect(focus).toBeCalled()
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { ReactNode, Component, ComponentClass, ReactElement, ClassType } from "react";
-
-const component: RegenComponent<{}, {name: string}> = function *({props, handler}) {
-  return null as any
-}
+import 'reflect-metadata'
+import { ReactNode, Component, ComponentClass, ReactElement, ClassType, createContext, createElement } from "react";
+import { unstable_batchedUpdates as batchedUpdates } from 'react-dom'
+import { Container, interfaces } from 'inversify'
 
 export type RegenComponent<P = {}, H = {}> = (ctx: RegenContext<P, H>) => Generator<ReactNode, unknown, P | null>
 
@@ -14,8 +13,15 @@ export type RegenContext<P, H = {}> = {
   afterLayout: (fn: RegenCallback, deps?: RegenCallbackDeps) => void,
   afterMount: (fn: RegenCallback) => void,
   afterUpdate: (fn: RegenCallback) => void,
-  inject: () => void,
-  provide: () => void,
+  withRender: (render: () => ReactNode) => Generator<ReactNode, unknown, P | null>
+  inject: {
+    <T>(ctr: new (...args: any[]) => T): T
+    <T, R>(type: T): R
+  },
+  provide: {
+    (ctr: new (...args: any[]) => any): void
+    (id: interfaces.ServiceIdentifier<unknown>, value: any): void
+  }
 }
 
 export type RegenCallbackDeps = () => Array<any>
@@ -36,18 +42,78 @@ const PropsAgentHandlers: ProxyHandler<Component> = {
   getOwnPropertyDescriptor(target, name) { return Reflect.getOwnPropertyDescriptor(target.props, name)},
 }
 
+
+let dirtyCmps: Component<any>[] = []
+let waitTicks: Array<() => void> = []
+let dirtySchedule: any = null
+let nextTick: (fn: () => void) => any
+
+if (typeof requestAnimationFrame === 'function') {
+  nextTick = requestAnimationFrame
+} else if (typeof Promise === 'function') {
+  console.log('promise')
+  nextTick = (fn) => Promise.resolve().then(fn)
+} else {
+  nextTick = (fn) => setTimeout(fn, 0)
+}
+
+function scheduleUpdateComponent(cmp: Component<any>) {
+  dirtyCmps.push(cmp)
+  if (!dirtySchedule) {
+    dirtySchedule = nextTick(flushDirtyComponent)
+  }
+}
+
+function flushDirtyComponent() {
+  batchedUpdates(() => {
+    const cmps = new Set(dirtyCmps.slice())
+    for (const cmp of cmps) {
+      cmp.forceUpdate()
+    }
+    dirtyCmps = []
+    dirtySchedule = null
+  })
+  for (const tick of waitTicks) {
+    tick()
+  }
+}
+
+export function waitTick(): Promise<void>
+export function waitTick(fn: () => void): void
+export function waitTick(fn?: () => void): Promise<void> | void {
+  if (fn) {
+    waitTicks.push(fn)
+    return
+  }
+  return new Promise((resolve) => waitTicks.push(resolve))
+}
+
+function * withRender<P>(render: () => ReactNode): Generator<ReactNode, void, P | null> {
+  for (let ret: P | null; ret = yield render();) {}
+}
+
+type DIContextType = {
+  container: Container | null
+}
+
+const DIContext = createContext<DIContextType>({
+  container: null
+})
+
 export function regen<P = {}, H = {}>(Cmp: RegenComponent<P, H>): ClassType<P, Component<P, any> & H, ComponentClass<P>> {
   class RegenedComponent extends Component<P, {count: number}> {
+    static contextType = DIContext
     static displayName = `${Cmp.name}(Regen)`
     state = {
       count: 1
     }
+    context!: DIContextType
     private propsAgent: P = new Proxy(this, PropsAgentHandlers) as any
     private ref: H = new Proxy(this, {}) as any
     private ctx: RegenContext<P, H> = {
       props: this.props,
       handler: this.ref,
-      mark: () => this.setState(({count}) => ({count: count + 1})),
+      mark: () => scheduleUpdateComponent(this),
       afterLayout: (fn, deps) => {
         this._$cbs.push({
           fn,
@@ -61,11 +127,24 @@ export function regen<P = {}, H = {}>(Cmp: RegenComponent<P, H>): ClassType<P, C
       afterUpdate: (fn) => {
         // this.ctx.afterRender(fn, () => ([!!this.state.count]))
       },
-      inject: () => {},
-      provide: () => {},
+      inject: (type: any) => {
+        return this.container.get(type)
+      },
+      provide: (type: any, func?: any) => {
+        if (!func) {
+          this.container.bind(type).to(type)
+          return
+        }
+        this.container.bind(type).to(func)
+        this.needProvide = true
+      },
+      withRender
     }
     private inst = Cmp(this.ctx)
     private _$cbs: Array<RegenCallbackType> = []
+    // TODO: 性能优化点
+    private container = this.context.container ? this.context.container.createChild() : new Container()
+    private needProvide = false
 
     componentWillUnmount() {
       this.inst.next(null)
@@ -74,6 +153,9 @@ export function regen<P = {}, H = {}>(Cmp: RegenComponent<P, H>): ClassType<P, C
     render() {
       const ret = this.inst.next(this.propsAgent)
       // TODO: check ret.done
+      if (this.needProvide || !this.context.container) {
+        return createElement(DIContext.Provider, {value: {container: this.container}}, ret.value as ReactElement)
+      }
       return ret.value as ReactElement
     }
   }
